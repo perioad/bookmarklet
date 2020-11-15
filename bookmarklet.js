aa6b7f30c15fc09d28dbd7c7781303d7825d6654
Perio Alexander <perioad@me.com>
2020-11-15 14:26:51 +0300

    Black theme, smiles, small text tweaks

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
 (function () {
   const paragraphs = document.getElementsByTagName('p');
   for (let paragraph of paragraphs) {
-    paragraph.style.background = 'coral';
+    paragraph.style.background = 'lightpink';
   }
 })();
